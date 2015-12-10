e2b9dd94caa282c2cf096e29ce5d35457c1ee328
csackrider <chad.sackrider@hobsons.com>
2015-12-10 11:07:48 -0500

    Fixing shit for tyler
    stuff

---
+++ src/test/java/stepDefs/Curriculum/Curriculum_StepDefs.java
@@ -0,0 +1,17 @@
+package stepDefs.Curriculum;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by csackrider on 12/10/2015.
+ */
+public class Curriculum_StepDefs {
+    @When("^I SSO into Curriculum as a counselor$")
+    public void I_SSO_into_Curriculum_as_a_counselor() throws Throwable {
+
+
+    }
+
+}
