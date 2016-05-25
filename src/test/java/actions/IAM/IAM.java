81bae46976d97a294c06511898330369f6c31564
Jaswinder Johal <jaswinder.johal@hobsons.com>
2016-05-25 15:42:21 -0400

    Refactored IAM java files

--- src/test/java/actions/IAM/IAM.java
+++ src/test/java/actions/IAM/IAM.java
@@ -1,6 +1,11 @@
 package actions.IAM;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import pageObjects.IAM.IAMPage;
 import stepDefs.Hooks;
 
 import static org.junit.Assert.assertTrue;
@@ -8,7 +13,14 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by franksejas on 1/22/2016.
  */
-public class IAM_Verify {
+public class IAM {
+    public static WebDriver driver;
+
+    public static void VerifyNotBeLogged() {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, IAMPage.class);
+        IAMPage.Login.isDisplayed();
+    }
 
     public static void verifyDashboard() throws InterruptedException {
         String recentupdates = Hooks.driver.findElement(By.id("course-management-container")).getText();
--- src/test/java/stepDefs/IAM/IAM_StepDefs.java
+++ src/test/java/stepDefs/IAM/IAM_StepDefs.java
@@ -1,6 +1,6 @@
 package stepDefs.IAM;
 
-import actions.IAM.IAM_Action;
+import actions.IAM.IAM;
 import actions.Login.Login;
 import cucumber.api.java.en.When;
 
@@ -12,7 +12,7 @@ public class IAM_StepDefs {
     @When("^I will not be logged into the district and not able to perform normal administrative functions$")
     public void i_will_not_be_logged_into_the_district_and_not_able_to_perform_normal_administrative_functions() throws Throwable {
 
-        IAM_Action.VerifyNotBeLogged();
+        IAM.VerifyNotBeLogged();
     }
 
     @When("^I will logged into the district and able to perform normal administrative functions$")
