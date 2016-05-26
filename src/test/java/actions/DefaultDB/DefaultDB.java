6e66b1c29a11203aaea1e48925a577250e8c53d3
Jaswinder Johal <jaswinder.johal@hobsons.com>
2016-05-26 13:44:38 -0400

    Refactored DefaultDB java files.

--- src/test/java/actions/DefaultDB/DefaultDB.java
+++ src/test/java/actions/DefaultDB/DefaultDB.java
@@ -1,16 +1,27 @@
 package actions.DefaultDB;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObjects.DefaultDB.DefaultDBPage;
 import stepDefs.Hooks;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by franksejas on 1/20/2016.
  */
-public class DefaultDB_Verify {
+public class DefaultDB {
+    public static WebDriver driver;
+
+    public static void  NavigateToCourses() {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, DefaultDBPage.class);
+        new WebDriverWait(Hooks.driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Courses")));
+        DefaultDBPage.Courses.click();
+    }
 
     public static void verifyHomePage() throws InterruptedException {
         String verifyHomeLink = Hooks.driver.findElement(By.linkText("home")).getText();
--- src/test/java/stepDefs/DefaultDB/DefaultDB_StepDefs.java
+++ src/test/java/stepDefs/DefaultDB/DefaultDB_StepDefs.java
@@ -1,7 +1,7 @@
 package stepDefs.DefaultDB;
 
-import actions.DefaultDB.DefaultDB_Action;
-import actions.DefaultDB.DefaultDB_Verify;
+import actions.DefaultDB.DefaultDB;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -13,16 +13,16 @@ public class DefaultDB_StepDefs {
     @When("^I verify that home page is displayed without errors$")
     public void i_verify_that_home_page_is_displayed_without_errors() throws Throwable {
 
-        DefaultDB_Verify.verifyHomePage();
+        DefaultDB.verifyHomePage();
     }
 
     @When("^I go to the Courses$")
     public void i_go_to_courses() throws Throwable {
-        DefaultDB_Action.NavigateToCourses();
+        DefaultDB.NavigateToCourses();
     }
 
     @Then("^I verify the Course Catalog page$")
     public void i_see_a_fatal_error_in_the_page() throws Throwable {
-        DefaultDB_Verify.verifyCourseCatalog();
+        DefaultDB.verifyCourseCatalog();
     }
 }
