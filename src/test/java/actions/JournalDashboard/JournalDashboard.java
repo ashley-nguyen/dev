784f4bb36714418691420994717f10cebab520e3
Jaswinder Johal <jaswinder.johal@hobsons.com>
2016-05-26 13:57:29 -0400

    Refactored JournalDashboard java files.

--- src/test/java/actions/JournalDashboard/JournalDashboard.java
+++ src/test/java/actions/JournalDashboard/JournalDashboard.java
@@ -1,16 +1,25 @@
 package actions.JournalDashboard;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import pageObjects.JournalDashboard.JournalDashboardPage;
 import stepDefs.Hooks;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by franksejas on 2/01/2016.
  */
-public class JournalDashboard_Verify {
+public class JournalDashboard {
     public static WebDriver driver;
+
+    public static void NavigateToJournalDashboard() {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, JournalDashboardPage.class);
+        JournalDashboardPage.School.click();
+        JournalDashboardPage.lnkJournalDashboard.click();
+    }
 
     public static void verifyJournalDashboard() throws InterruptedException {
         String dataVerification = Hooks.driver.findElement(By.className("pageheader")).getText();
--- src/test/java/stepDefs/JournalDashboard/JournalDashboard_StepDefs.java
+++ src/test/java/stepDefs/JournalDashboard/JournalDashboard_StepDefs.java
@@ -1,7 +1,6 @@
 package stepDefs.JournalDashboard;
 
-import actions.JournalDashboard.JournalDashboard_Action;
-import actions.JournalDashboard.JournalDashboard_Verify;
+import actions.JournalDashboard.JournalDashboard;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -13,11 +12,11 @@ public class JournalDashboard_StepDefs {
     @When("^I go to Journal Dashboard$")
     public void i_go_to_application_manager() throws Throwable {
 
-        JournalDashboard_Action.NavigateToJournalDashboard();
+        JournalDashboard.NavigateToJournalDashboard();
     }
 
     @Then("^I will see data in Journal Dashboard")
     public void i_will_see_data_in_journal_dashboard() throws Throwable {
-        JournalDashboard_Verify.verifyJournalDashboard();
+        JournalDashboard.verifyJournalDashboard();
     }
 }
