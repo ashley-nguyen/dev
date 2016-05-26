5adabda641300ac4c38cb2f7b801de566570ea63
Jaswinder Johal <jaswinder.johal@hobsons.com>
2016-05-26 10:14:59 -0400

    Refactored Assessments files

--- src/test/java/actions/Assessments/Assessments.java
+++ src/test/java/actions/Assessments/Assessments.java
@@ -1,5 +1,7 @@
 package actions.Assessments;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -11,7 +13,7 @@ import stepDefs.Hooks;
 /**
  * Created by franksejas on 1/14/2016.
  */
-public class Assessments_Action {
+public class Assessments {
     public static WebDriver driver;
 
     public static void ClickOnAssessments() throws InterruptedException {
@@ -19,5 +21,10 @@ public class Assessments_Action {
         PageFactory.initElements(driver, AssessmentsPage.class);
         new WebDriverWait(Hooks.driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Assessments")));
         AssessmentsPage.Assessments.click();
+    }
+
+    public static void verifyAssessment() throws InterruptedException {
+        String dataVerification = Hooks.driver.findElement(By.className("student-assessments")).getText();
+        assertTrue("Error Verification!", !dataVerification.contains("Whoops, looks like something went wrong."));
     }
 }
--- src/test/java/stepDefs/Assessments/Assessments_StepDefs.java
+++ src/test/java/stepDefs/Assessments/Assessments_StepDefs.java
@@ -1,7 +1,7 @@
 package stepDefs.Assessments;
 
-import actions.Assessments.Assessments_Action;
-import actions.Assessments.Assessments_Verify;
+import actions.Assessments.Assessments;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -12,11 +12,11 @@ public class Assessments_StepDefs {
 
     @When("^I click on Assessments tab$")
     public void i_click_on_assessments_tab() throws Throwable {
-        Assessments_Action.ClickOnAssessments();
+        Assessments.ClickOnAssessments();
     }
 
     @Then("^I should not see errors in Assessments$")
     public void i_should_not_see_errors_in_assessments() throws Throwable {
-        Assessments_Verify.verifyAssessment();
+        Assessments.verifyAssessment();
     }
 }
