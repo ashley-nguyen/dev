f61cf13b6710cdc1cba70656dc21a57c5641261b
Jaswinder Johal <jaswinder.johal@hobsons.com>
2016-05-26 13:40:00 -0400

    Refactored curriculum files.

--- src/test/java/actions/Curriculum/Curriculum.java
+++ src/test/java/actions/Curriculum/Curriculum.java
@@ -1,22 +1,146 @@
 package actions.Curriculum;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObjects.Curriculum.CurriculumPage;
+import pageObjects.Header.SchoolPageHeader;
 import stepDefs.Hooks;
-
-import java.util.List;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by ttremblay on 12/10/15.
  */
-public class Curriculum_Verify {
+public class Curriculum {
     public static WebDriver driver;
 
+    public static void SSOtocurriculum() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, SchoolPageHeader.class);
+        SchoolPageHeader.lnkNavianceCurriculum.click();
+    }
+
+    public static void navtoclasses() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkClasses.click();
+
+    }
+
+
+    public static void studentsdropdown() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkStudents.click();
+    }
+
+    public static void navtoactivity() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkActivity.click();
+    }
+
+    public static void navtoprogress() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkProgress.click();
+
+    }
+
+    public static void NavToThemes() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.ButtonExploreThemes.click();
+    }
+
+    public static void NavToScopeAndSequence() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkScopeAndSequence.click();
+    }
+
+    public static void SixthGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectSixthGrade.click();
+    }
+
+    public static void ClickGoToLevel() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.GoToLevel.click();
+    }
+
+    public static void SeventhGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectSeventhGrade.click();
+
+    }
+
+    public static void EigthGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectEigthGrade.click();
+    }
+    public static void NinthGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectNinthGrade.click();
+    }
+
+    public static void TenthGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectTenthGrade.click();
+    }
+    public static void EleventhGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectEleventhGrade.click();
+
+    }
+    public static void TwelfthGrade() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.SelectTwelfthGrade.click();
+    }
+
+    public static void ClickSelfDiscovery() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkSelfDiscovery.click();
+    }
+    public static void ClickSuccessSkills() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkSuccessSkills.click();
+    }
+    public static void ClickSupportNetworks() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkSupportNetworks.click();
+    }
+    public static void ClickCollegePlanning() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkCollegePlanning.click();
+    }
+    public static void ClickCareerPlanning() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkCareerPlanning.click();
+    }
+    public static void ClickFinancialPlanning() throws InterruptedException {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, CurriculumPage.class);
+        CurriculumPage.lnkFinancialPLanning.click();
+    }
 
     public static void verifycurriculumhomepage(String strText) throws InterruptedException {
         driver = Hooks.driver;
@@ -130,6 +254,7 @@ public class Curriculum_Verify {
 
         assertTrue("header text exists", h4Exists);
     }
+
     public static void VerifyGrade9(String strText) throws InterruptedException {
         driver = Hooks.driver;
         System.out.println(driver.getPageSource());
@@ -204,6 +329,7 @@ public class Curriculum_Verify {
 
 
     }
+
     public static void VerifyGrade12(String strText) throws InterruptedException {
         driver = Hooks.driver;
         System.out.println(driver.getPageSource());
@@ -228,6 +354,7 @@ public class Curriculum_Verify {
 
 
     }
+
     public static void VerifySelfDiscovery(String strText) throws InterruptedException {
         driver = Hooks.driver;
         System.out.println(driver.getPageSource());
@@ -250,6 +377,7 @@ public class Curriculum_Verify {
 
         assertTrue("header text exists", h4Exists);
     }
+
     public static void VerifySuccessSkills(String strText) throws InterruptedException {
         driver = Hooks.driver;
         System.out.println(driver.getPageSource());
@@ -272,6 +400,7 @@ public class Curriculum_Verify {
 
         assertTrue("header text exists", h4Exists);
     }
+
     public static void VerifySupportNetworks(String strText) throws InterruptedException {
         driver = Hooks.driver;
         System.out.println(driver.getPageSource());
@@ -345,6 +474,7 @@ public class Curriculum_Verify {
         assertTrue("header text exists", h4Exists);
 
     }
+
     public static void VerifyFinancialPlanning(String strText) throws InterruptedException {
         driver = Hooks.driver;
         System.out.println(driver.getPageSource());
@@ -368,10 +498,4 @@ public class Curriculum_Verify {
         assertTrue("header text exists", h4Exists);
 
     }
-
 }
-
-
-
-
-
--- src/test/java/stepDefs/Curriculum/Curriculum_StepDefs.java
+++ src/test/java/stepDefs/Curriculum/Curriculum_StepDefs.java
@@ -1,13 +1,11 @@
 package stepDefs.Curriculum;
 
-import actions.Curriculum.Curriculum_Action;
-import actions.Curriculum.Curriculum_Verify;
-import cucumber.api.PendingException;
+import actions.Curriculum.Curriculum;
+
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import pageObjects.Curriculum.CurriculumPage;
 
 /**
  * Created by csackrider on 12/10/2015.
@@ -15,194 +13,194 @@ import pageObjects.Curriculum.CurriculumPage;
 public class Curriculum_StepDefs {
     @When("^I SSO into Curriculum as a counselor$")
     public void I_SSO_into_Curriculum_as_a_counselor() throws Throwable {
-        Curriculum_Action.SSOtocurriculum();
+        Curriculum.SSOtocurriculum();
     }
 
     @Then("^I will see the Scope and Sequence page$")
     public void iWillSeeTheScopeAndSequencePage() throws Throwable {
-        Curriculum_Verify.verifycurriculumhomepage("SCOPE AND SEQUENCE");
+        Curriculum.verifycurriculumhomepage("SCOPE AND SEQUENCE");
     }
 
     @Then("I click the Students dropdown$")
     public void iclickthestudentsdropdown() throws Throwable {
-        Curriculum_Action.studentsdropdown();
+        Curriculum.studentsdropdown();
 
     }
 
     @When("In the Students dropdown I click Classes$")
     public void inthestudentsdropdowniclickclasses() throws Throwable {
-        Curriculum_Action.navtoclasses();
+        Curriculum.navtoclasses();
 
     }
 
 
     @Then("^I will see the Classes page$")
     public void iwillseetheclassespage() throws Throwable {
-        Curriculum_Verify.verifyclassespage("+ Add Class");
+        Curriculum.verifyclassespage("+ Add Class");
     }
 
     @Then("^I will see the Activity page$")
     public void iwillseetheactivitypage() throws Throwable {
-        Curriculum_Verify.verifyactivitypage("View Plans");
+        Curriculum.verifyactivitypage("View Plans");
     }
 
     @And("^In the Students dropdown I click Activity$")
     public void iclickactivity() throws Throwable {
-        Curriculum_Action.navtoactivity();
+        Curriculum.navtoactivity();
     }
 
     @When("In the students dropdown I click Progress$")
     public void inthestudentsdropdowniclickprogress() throws Throwable {
-        Curriculum_Action.navtoprogress();
+        Curriculum.navtoprogress();
 
     }
 
     @Then("^I will see the Progress page$")
     public void iwillseetheprogresspage() throws Throwable {
-        Curriculum_Verify.verifyprogresspage("spacious");
+        Curriculum.verifyprogresspage("spacious");
 
     }
 
     @When("I click the Explore Themes button")
     public void IClickExploreThemes() throws Throwable {
-        Curriculum_Action.NavToThemes();
+        Curriculum.NavToThemes();
     }
 
     @Then("^I will see the Our Themes page$")
     public void ISeeOurThemes() throws Throwable {
-        Curriculum_Verify.VerifyOurThemesPage("Explore Self Discovery");
+        Curriculum.VerifyOurThemesPage("Explore Self Discovery");
     }
 
     @Given("I Navigate to the Scope and Sequence page$")
     public void inavtoscopeandsequence() throws Throwable {
-        Curriculum_Action.NavToScopeAndSequence();
+        Curriculum.NavToScopeAndSequence();
     }
 
     @When("I select 6th grade lesson sequence")
     public void ISelect6thGrade() throws Throwable {
-        Curriculum_Action.SixthGrade();
+        Curriculum.SixthGrade();
     }
 
     @And("Click go to level")
     public void ClickGoToLevel() throws Throwable {
-        Curriculum_Action.ClickGoToLevel();
+        Curriculum.ClickGoToLevel();
 
     }
 
     @Then("I will see the 6th grade lesson sequence")
     public void VerifyGrade6() throws Throwable {
-        Curriculum_Verify.VerifyGrade6("Grade 6");
+        Curriculum.VerifyGrade6("Grade 6");
 
     }
 
     @When("I select 7th grade lesson sequence")
     public void ISelect7thGrade() throws Throwable {
-        Curriculum_Action.SeventhGrade();
+        Curriculum.SeventhGrade();
     }
 
     @Then("I will see the 7th grade lesson sequence")
     public void VerifyGrade7() throws Throwable {
-        Curriculum_Verify.VerifyGrade7("Grade 7");
+        Curriculum.VerifyGrade7("Grade 7");
 
     }
 
     @When("I select 8th grade lesson sequence")
     public void ISelect8thGrade() throws Throwable {
-        Curriculum_Action.EigthGrade();
+        Curriculum.EigthGrade();
     }
 
     @Then("I will see the 8th grade lesson sequence")
     public void VerifyGrade8() throws Throwable {
-        Curriculum_Verify.VerifyGrade8("Grade 8");
+        Curriculum.VerifyGrade8("Grade 8");
     }
 
     @When("I select 9th grade lesson sequence")
     public void ISelect9thGrade() throws Throwable {
-        Curriculum_Action.NinthGrade();
+        Curriculum.NinthGrade();
     }
     @Then("I will see the 9th grade lesson sequence")
     public void VerifyGrade9() throws Throwable {
-        Curriculum_Verify.VerifyGrade9("Grade 9");
+        Curriculum.VerifyGrade9("Grade 9");
     }
     @When("I select 10th grade lesson sequence")
     public void ISelect10thGrade() throws Throwable {
-        Curriculum_Action.TenthGrade();
+        Curriculum.TenthGrade();
     }
     @Then("I will see the 10th grade lesson sequence")
     public void VerifyGrade10() throws Throwable {
-        Curriculum_Verify.VerifyGrade10("Grade 10");
+        Curriculum.VerifyGrade10("Grade 10");
 
     }
 
     @When("I select 11th grade lesson sequence")
     public void ISelect11thGrade() throws Throwable {
-        Curriculum_Action.EleventhGrade();
+        Curriculum.EleventhGrade();
     }
     @Then("I will see the 11th grade lesson sequence")
     public void VerifyGrade11() throws Throwable {
-        Curriculum_Verify.VerifyGrade11("Grade 11");
+        Curriculum.VerifyGrade11("Grade 11");
     }
     @When("I select 12th grade lesson sequence")
     public void ISelect12thGrade() throws Throwable {
-        Curriculum_Action.TwelfthGrade();
+        Curriculum.TwelfthGrade();
     }
     @Then("I will see the 12th grade lesson sequence")
     public void VerifyGrade12() throws Throwable {
-        Curriculum_Verify.VerifyGrade12("Grade 12");
+        Curriculum.VerifyGrade12("Grade 12");
     }
 
     @When("I select Self Discovery theme")
     public void SelectSelfDiscovery() throws Throwable {
-        Curriculum_Action.ClickSelfDiscovery();
+        Curriculum.ClickSelfDiscovery();
     }
 
     @Then("I will see the Self Discovery lessons")
     public void VerifySelfDiscovery() throws Throwable {
-        Curriculum_Verify.VerifySelfDiscovery("Self Discovery");
+        Curriculum.VerifySelfDiscovery("Self Discovery");
     }
     @When("I select Success Skills theme")
     public void SelectSuccessSkills() throws Throwable {
-        Curriculum_Action.ClickSuccessSkills();
+        Curriculum.ClickSuccessSkills();
     }
 
     @Then("I will see the Success Skills lessons")
     public void VerifySuccessSkills() throws Throwable {
-        Curriculum_Verify.VerifySuccessSkills("Success Skills");
+        Curriculum.VerifySuccessSkills("Success Skills");
     }
     @When("I select Support Networks theme")
     public void SelectSupportNetworks() throws Throwable {
-        Curriculum_Action.ClickSupportNetworks();
+        Curriculum.ClickSupportNetworks();
 
     }
     @Then("I will see the Support Networks lessons")
     public void VerifySupportNetworks() throws Throwable {
-        Curriculum_Verify.VerifySupportNetworks("Support Network");
+        Curriculum.VerifySupportNetworks("Support Network");
     }
     @When("I select College Planning theme")
     public void SelectCollegePlanning() throws Throwable {
-        Curriculum_Action.ClickCollegePlanning();
+        Curriculum.ClickCollegePlanning();
     }
     @Then("I will see the College Planning lessons")
     public void VerifyCollegePlanning() throws Throwable {
-        Curriculum_Verify.VerifyCollegePlanning("College Planning");
+        Curriculum.VerifyCollegePlanning("College Planning");
     }
     @When("I select Career Planning theme")
     public void SelectCareerPlanning() throws Throwable {
-        Curriculum_Action.ClickCareerPlanning();
+        Curriculum.ClickCareerPlanning();
     }
     @Then("I will see the Career Planning lessons")
     public void VerifyCareerPlanning() throws Throwable {
-        Curriculum_Verify.VerifyCareerPlanning("Career Planning");
+        Curriculum.VerifyCareerPlanning("Career Planning");
     }
 
     @When("I select Financial Planning theme")
     public void SelectFinancialPlanning() throws Throwable {
-        Curriculum_Action.ClickFinancialPlanning();
+        Curriculum.ClickFinancialPlanning();
     }
 
     @Then("I will see the Financial Planning lessons")
     public void VerifyFinancialPlanning() throws Throwable {
-        Curriculum_Verify.VerifyFinancialPlanning("Financial Planning");
+        Curriculum.VerifyFinancialPlanning("Financial Planning");
     }
 
 
