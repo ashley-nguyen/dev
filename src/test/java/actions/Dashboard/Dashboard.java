2d50c35977570c424443fc689db1fd71f3b1c577
Jaswinder Johal <jaswinder.johal@hobsons.com>
2016-05-26 13:42:27 -0400

    Refactored Dashboard files.

--- src/test/java/actions/Dashboard/Dashboard.java
+++ src/test/java/actions/Dashboard/Dashboard.java
@@ -1,16 +1,28 @@
 package actions.Dashboard;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import stepDefs.Hooks;
 
-import static org.junit.Assert.assertTrue;
+import pageObjects.Dashboard.DashboardPage;
+import stepDefs.Hooks;
 
 /**
  * Created by franksejas on 12/22/2015.
  */
-public class DashboardVerify {
+public class Dashboard {
+    public static WebDriver driver;
+
+    public static void NavigateToDashboard() {
+        driver = Hooks.driver;
+        PageFactory.initElements(driver, DashboardPage.class);
+        DashboardPage.School.click();
+        DashboardPage.Planner.click();
+    }
 
     public static void verifyDashboard() throws InterruptedException {
         new WebDriverWait(Hooks.driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("course-management-container")));
--- src/test/java/stepDefs/Dashboard/Dashboard_StepDefs.java
+++ src/test/java/stepDefs/Dashboard/Dashboard_StepDefs.java
@@ -1,7 +1,6 @@
 package stepDefs.Dashboard;
 
-import actions.Dashboard.DashboardAction;
-import actions.Dashboard.DashboardVerify;
+import actions.Dashboard.Dashboard;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -13,11 +12,11 @@ public class Dashboard_StepDefs {
     @When("^I go to the Planner dashboard$")
     public void i_go_to_planner_dashboard() throws Throwable {
 
-        DashboardAction.NavigateToDashboard();
+        Dashboard.NavigateToDashboard();
     }
 
     @Then("^I will see data in recent updates$")
     public void i_will_see_data_in_recent_updates() throws Throwable {
-        DashboardVerify.verifyDashboard();
+        Dashboard.verifyDashboard();
     }
 }
